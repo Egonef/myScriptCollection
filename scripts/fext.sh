5baeceec0dc3b1b521c3a071b073b0f0b574cc6b
Emilio Gomez Nef <126122650+Egonef@users.noreply.github.com>
2023-09-21 13:50:54 +0200

    Improve fext

--- scripts/fext.sh
+++ scripts/fext.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
-#Set the file in the first argument as a variable
-file="$1"
+# Check if file argument is provided
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <file1> [<file2> ...]"
+    exit 1
+fi
 
+# Loop over all the files provided as arguments
+for file in "$@"; do
 
-#Check if file has a supported extension
-
-if  [ -z "$file" ]; then
-    echo "You are missing an argument (Filename)"
-else
-    
-    if [[ "$file" == *".zip"* ]] ; then
+    # Check if file has a supported extension
+    if [[ "$file" == *".zip"* ]]; then
         unzip "$file"
-
-    elif [[ "$file" == *".tar"* ]] ; then
+    elif [[ "$file" == *".tar"* ]]; then
         tar -xf "$file"
-
-    elif [[ "$file" == *".rar"* ]] ; then
+    elif [[ "$file" == *".rar"* ]]; then
         unrar x "$file"
-
     else
-        echo "This type of file is not supported yet"
-        exit
+        echo "Error: $file is not a supported file type"
+        continue
     fi
 
-    echo "File extracted"
+    # Check if extraction was successful
+    if [ $? -eq 0 ]; then
+        echo "File extracted: $file"
+    else
+        echo "Error extracting file: $file"
+    fi
 
-fi
+done
